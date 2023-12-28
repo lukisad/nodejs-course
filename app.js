b1b439da3dbd13cd1c07000a7654c950f4761abe
Łukasz Sadowski <lukasz.sadowski@asdirect.pl>
2023-12-28 11:53:58 +0100

    Dodanie filtrowania ściezek

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Uzycie obiektu router'a
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+// Obsługa strony 404
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
+});
 
 // tworzenie i nasłuchiwanie serwera
 app.listen(3000);
